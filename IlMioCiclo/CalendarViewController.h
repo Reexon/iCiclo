339465bcc520a75656068ef0fd91b93fe8bf28c3
Loris <loris@reexon.net>
2014-05-16 15:11:06 +0200

    Classe Calendario

---
+++ IlMioCiclo/CalendarViewController.h
@@ -0,0 +1,14 @@
+//
+//  CalendarViewController.h
+//  IlMioCiclo
+//
+//  Created by Loris D'antonio on 16/05/14.
+//  Copyright (c) 2014 Marco Velluto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UICollectionViewController
+@property (strong, nonatomic) IBOutlet UICollectionView *calendar;
+
+@end
