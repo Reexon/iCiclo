caaa3e2615a3f78186a51804da542b16b5e97dd0
Loris <loris@reexon.net>
2014-05-16 18:17:11 +0200

    Visualizzazione Giorni Calendario

--- IlMioCiclo/CalendarViewController.m
+++ IlMioCiclo/CalendarViewController.m
@@ -29,10 +29,68 @@
 {
     monthArray = @[@"Gennaio",@"Febbraio",@"Marzo",@"Aprile",@"Maggio",@"Giugno",@"Luglio",@"Agosto",@"Settembre",@"Ottobre",@"Novembre",@"Dicembre"];
     [super viewDidLoad];
+    
+    
 
+    
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"GMT"];
+    NSDateFormatter *day = [[NSDateFormatter alloc] init];
+    day.timeStyle = NSDateFormatterNoStyle;
+    day.dateFormat = @"EEEE";
+    [day setCalendar:gregorianCalendar];
+    [day setTimeZone:timeZone];
+    
+    NSDateFormatter *date = [[NSDateFormatter alloc] init];
+    date.timeStyle = NSDateFormatterNoStyle;
+    date.dateFormat = @"dd/MM/yyyy";
+    [date setCalendar:gregorianCalendar];
+    [date setTimeZone:timeZone];
+    
+    NSDate *data = [date dateFromString:@"22/11/2014"];
+
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    NSDateComponents* comp = [cal components:NSWeekdayCalendarUnit fromDate:data];
+
+    NSString *day_name = [day stringFromDate:data];
+    NSLog(@"%@ %ld",data,    (long)[comp weekday]);
     // Do any additional setup after loading the view.
 }
 
+- (int)daysInMonth:(int)month{
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"GMT"];
+    NSDateFormatter *date = [[NSDateFormatter alloc] init];
+    date.timeStyle = NSDateFormatterNoStyle;
+    date.dateFormat = @"dd/MM/yyyy";
+    [date setCalendar:gregorianCalendar];
+    [date setTimeZone:timeZone];
+    
+    NSDate *data = [date dateFromString:[NSString stringWithFormat:@"01/%d/2014",month]];
+
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSRange days = [gregorian rangeOfUnit:NSDayCalendarUnit inUnit:NSMonthCalendarUnit forDate:data];
+    int daysInGivenMonth= days.length;
+    
+    return daysInGivenMonth;
+}
+
+- (int)dayOfWeek:(int)day withMonth:(int)month withYear:(int)year{
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"GMT"];
+    NSDateFormatter *date = [[NSDateFormatter alloc] init];
+    date.timeStyle = NSDateFormatterNoStyle;
+    date.dateFormat = @"dd/MM/yyyy";
+    [date setCalendar:gregorianCalendar];
+    [date setTimeZone:timeZone];
+    NSDate *data = [date dateFromString:[NSString stringWithFormat:@"%d/%d/%d",day,month,year]];
+    
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    NSDateComponents* comp = [cal components:NSWeekdayCalendarUnit fromDate:data];
+    
+    return [comp weekday];
+
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -58,6 +116,32 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     MonthCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"identifier" forIndexPath:indexPath];
     cell.monthLabel.text =[monthArray objectAtIndex:indexPath.row];
+    
+    int numberOfDays = [self daysInMonth:(indexPath.row+1)];
+    for (int i = 1, y = 50; i <= numberOfDays ; i++){
+        
+        int day = [self dayOfWeek:i withMonth:indexPath.row+1 withYear:2014];
+        if(day == 1)
+            day = 6;
+        else
+            day -=2;
+        
+        if (day == 0) {
+            y+=20;
+        }
+        // creo la label che rappresenta il numero del giorno
+        UILabel  * label = [[UILabel alloc] initWithFrame:CGRectMake(4+day*21, y, 20, 21)];
+        label.font = [UIFont fontWithName:@"Arial" size:9.0f];
+        label.backgroundColor = [UIColor clearColor];
+        label.textColor=[UIColor blackColor
+                         ];
+        label.numberOfLines=1;
+        label.text = [NSString stringWithFormat:@"%d",i];
+        [cell.contentView addSubview:label];
+    }
+    
+    
+    
     return cell;
 }
 @end
